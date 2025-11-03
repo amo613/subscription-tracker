7473d62c6f566f17a843565773cd33acfa64b175
arminajdarpasic <arminajdarpasic6@gmail.com>
2025-11-03 12:42:13 +0100

    Changed just a small thing

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,9 +2,10 @@ import { Router } from 'express';
 import {getUser, getUsers} from "../controllers/user.controller.js";
 import authorize from "../middlewares/auth.middleware.js";
 const userRouter = Router();
+import errorMiddleware from "../middlewares/error.middleware.js";
 
-userRouter.get('/', getUsers);
-userRouter.get('/:id', authorize, getUser);
+userRouter.get('/',authorize, getUsers);
+userRouter.get('/:id', authorize,errorMiddleware, getUser);
 userRouter.post('/', (req, res) => res.send({ title: 'POST new user' }));
 userRouter.put('/:id', (req, res) => res.send({ title: 'PUT user by ID', id: req.params.id }));
 userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user by ID', id: req.params.id }));
